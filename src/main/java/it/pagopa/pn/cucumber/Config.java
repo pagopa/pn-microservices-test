78c6e88e85164c7c05c7c2cd50b9548f1c5fe904
MartinaMattei <martinamattei.mm00@gmail.com>
2024-03-08 10:21:01 +0100

    PN-10186
    PN-10186 spostato test cucumber in repository

---
+++ src/main/java/it/pagopa/pn/cucumber/Config.java
@@ -0,0 +1,51 @@
+package it.pagopa.pn.cucumber;
+
+import lombok.CustomLog;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
+@CustomLog
+public class Config {
+
+    private static Config instance = null;
+
+    private String baseUrl;
+
+    private Config(){
+        try {
+
+        	baseUrl = System.getProperty("baseURL");
+        	
+        	if( Objects.isNull(baseUrl) ) {
+	            Properties prop = new Properties();
+	            InputStream fileStream = this.getClass().getClassLoader().getResourceAsStream("application-test.properties");
+	            if(fileStream == null){
+	                log.error("File properties non trovato");
+	                System.exit(1);
+	            }
+	            prop.load(fileStream);
+	            this.baseUrl = prop.getProperty("baseURL");
+        	}
+            
+        } catch (IOException ex) {
+            log.error("Errore nel caricamento delle properties -> " + ex.getMessage());
+            System.exit(1);
+        }
+    }
+
+
+    public static Config getInstance() {
+        if (Config.instance == null) {
+            Config.instance = new Config();
+        }
+
+        return Config.instance;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
