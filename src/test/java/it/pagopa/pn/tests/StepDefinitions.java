8dd342cf3a3e8ee30c0c71403570fcec3dc43ac3
MartinaMattei <martinamattei.mm00@gmail.com>
2024-04-03 15:29:34 +0200

    PN-10189 fix debug log

--- src/test/java/it/pagopa/pn/tests/StepDefinitions.java
+++ src/test/java/it/pagopa/pn/tests/StepDefinitions.java
@@ -1,5 +1,7 @@
 package it.pagopa.pn.tests;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.amazonaws.services.kms.model.NotFoundException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -19,8 +21,8 @@ import it.pagopa.pn.cucumber.SafeStorageUtils;
 import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.FileDownloadResponse;
 import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.UpdateFileMetadataRequest;
 import lombok.CustomLog;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
@@ -37,7 +39,7 @@ import java.util.Date;
 import static it.pagopa.pn.cucumber.SqsUtils.checkIfDocumentIsAvailable;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@CustomLog
+@Slf4j
 public class StepDefinitions {
     private String sPNClient = null;
     private String sPNClient_AK = null;
@@ -62,9 +64,11 @@ public class StepDefinitions {
 
 
 
+
     @BeforeAll
     public static void loadPropertiesForQueue() {
         nomeCoda = Config.getInstance().getNomeCoda();
+
     }
 
     @Given("{string} authenticated by {string} try to upload a document of type {string} with content type {string} using {string}")
@@ -75,7 +79,6 @@ public class StepDefinitions {
         sDocumentType = parseIfTagged(sDocumentType);
         sMimeType = parseIfTagged(sMimeType);
         sFileName = parseIfTagged(sFileName);
-
         log.debug("pn-client {}", sPNClient);
 
         Path pathFile = Paths.get(sFileName).toAbsolutePath();
@@ -172,9 +175,9 @@ public class StepDefinitions {
 
        oResp = SafeStorageUtils.getPresignedURLUpload(sPNClient, sPNClient_AK, sMimeType, sDocumentType, sSHA256, sMD5, "SAVED", boHeader, Checksum.SHA256);
 
-
         iRC = oResp.getStatusCode();
         log.debug("oResp body: " + oResp.getBody().asString());
+
         log.debug("oResp uploadUrl: " + oResp.then().extract().path("uploadUrl"));
         log.info("fileKey: " + oResp.then().extract().path("key"));
         log.debug("oResp secret: " + oResp.then().extract().path("secret"));
