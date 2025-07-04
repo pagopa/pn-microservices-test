6e9d3cba1dac8140e6f1ebd15b22ae7046233a6b
damiano.lozzi <damianolozzi1989@gmail.com>
2025-07-04 15:36:55 +0200

    feature/PN-15215: add sendPaperMessageWithDocumentTransformationTypeAndRasterizationFlag

--- src/main/java/it/pagopa/pn/cucumber/utils/ExternalChannelUtils.java
+++ src/main/java/it/pagopa/pn/cucumber/utils/ExternalChannelUtils.java
@@ -91,6 +91,26 @@ public class ExternalChannelUtils extends RequestTemplate {
         return CommonUtils.myPut(oReq, RequestEndpoint.CARTACEO_ENDPOINT, CommonUtils.PN_EC);
     }
 
+    public static Response sendPaperMessageWithDocumentTransformationTypeAndRasterizationFlag(String clientId, String requestId, List<PnAttachment> attachments, String transformationDocumentType,boolean applyRasterizationFlag) {
+        RequestSpecification oReq = stdReq()
+                .header(X_PAGOPA_EXTCH_CX_ID, clientId)
+                .pathParam(REQUEST_IDX, requestId);
+        PaperEngageRequest paperEngageRequest = createPaperEngageRequest(requestId);
+        paperEngageRequest.setTransformationDocumentType(transformationDocumentType);
+        paperEngageRequest.applyRasterization(applyRasterizationFlag);
+        List<PaperEngageRequestAttachments> paperEngageRequestAttachmentsList = attachments.stream().map(attachment -> {
+            PaperEngageRequestAttachments paperEngageRequestAttachments = new PaperEngageRequestAttachments();
+            paperEngageRequestAttachments.setDocumentType(attachment.getDocumentType());
+            paperEngageRequestAttachments.setUri(attachment.getUri());
+            paperEngageRequestAttachments.setSha256(attachment.getSha256());
+            paperEngageRequestAttachments.setOrder(BigDecimal.ZERO);
+            return paperEngageRequestAttachments;
+        }).toList();
+        paperEngageRequest.setAttachments(paperEngageRequestAttachmentsList);
+        oReq.body(paperEngageRequest);
+        return CommonUtils.myPut(oReq, RequestEndpoint.CARTACEO_ENDPOINT, CommonUtils.PN_EC);
+    }
+
     public static Response sendPaperMessageRasterFlag(String clientId, String requestId, String requestPaId, String applyRasterization, List<PnAttachment> attachments) {
         log.info("sendPaperMessageRasterFlag {}", applyRasterization);
         RequestSpecification oReq = stdReq()
