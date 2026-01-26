b4ca8db1d609e13ffd6b2962b1dbdd2e95e021a7
Martina Mattei <martinamattei.mm00@gmail.com>
2026-01-26 16:58:40 +0100

    PN-17454: Add cases for chain transformation

--- src/main/java/it/pagopa/pn/service/S3Service.java
+++ src/main/java/it/pagopa/pn/service/S3Service.java
@@ -1,7 +1,13 @@
 package it.pagopa.pn.service;
 
+import software.amazon.awssdk.services.s3.model.GetObjectTaggingResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectVersionsResponse;
+
 public interface S3Service {
 
     String getBucketName(String prefix);
+    ListObjectVersionsResponse listObjectVersions(String key, String bucket);
+
+
 
 }
